8adb2750e95ff78b582610ebf1c7657e8015a3f8
DonizeteVida <donizetevida1998@outlook.com>
2024-11-22 01:21:13 -0300

    Basic permission prompt

--- app/src/main/java/com/any/musica/MainActivity.kt
+++ app/src/main/java/com/any/musica/MainActivity.kt
@@ -1,15 +1,23 @@
- package com.any.musica
+package com.any.musica
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.any.musica.ui.theme.MusicaTheme
 
@@ -20,6 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MusicaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    RequestMusicaPermissions()
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
@@ -27,6 +36,48 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun RequestMusicaPermissions() {
+    PermissionRequester(
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            listOf(
+                android.Manifest.permission.POST_NOTIFICATIONS,
+                android.Manifest.permission.READ_MEDIA_AUDIO
+            )
+        } else {
+            listOf(
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        }
+    )
+}
+
+@Composable
+fun PermissionRequester(
+    vararg permissions: List<String>
+) {
+    val context = LocalContext.current
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions(),
+        onResult = onResult@{ isGranted ->
+            if (isGranted.values.all { it }) return@onResult
+            context.startActivity(
+                Intent(
+                    Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                ).setData(
+                    Uri.fromParts("package", context.packageName, null)
+                )
+            )
+        }
+    )
+
+    LaunchedEffect(permissions) {
+        permissions
+            .flatMap { it }
+            .toTypedArray().also(permissionLauncher::launch)
     }
 }
 
