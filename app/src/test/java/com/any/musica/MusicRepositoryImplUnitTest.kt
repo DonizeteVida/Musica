517d53c4120081875206acbfd1e22eae0873ee9c
DonizeteVida <donizetevida1998@outlook.com>
2024-11-25 22:24:32 -0300

    Multiple cursor rows test

--- app/src/test/java/com/any/musica/MusicRepositoryImplUnitTest.kt
+++ app/src/test/java/com/any/musica/MusicRepositoryImplUnitTest.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.doReturnConsecutively
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
@@ -39,5 +40,28 @@ class MusicRepositoryImplUnitTest {
         verify(cursor, times(2)).moveToNext()
         verify(cursor, times(1)).getColumnIndex(ID)
         verify(cursor, times(1)).getString(ID_COLUMN)
+    }
+
+    @Test
+    fun `it parses a multiple cursor row`() = runTest {
+        val n = (2..100).random()
+
+        val cursor = mock<Cursor> {
+            on { moveToNext() } doReturnConsecutively List(n) { true } + false
+            on { getColumnIndex(ID) } doReturn ID_COLUMN
+            on { getString(ID_COLUMN) } doReturnConsecutively List(n) { "$FAKE_URI$it" }
+        }
+        val contentResolverQuery = mock<ContentResolverQuery> {
+            onBlocking { getMusicCursor() } doReturn cursor
+        }
+        val target = MusicRepositoryImpl(contentResolverQuery)
+
+        val actual = target.getAll()
+        val expected = List(n) { Music(id = "$FAKE_URI$it") }
+        assertEquals(expected, actual)
+
+        verify(cursor, times(n + 1)).moveToNext()
+        verify(cursor, times(1)).getColumnIndex(ID)
+        verify(cursor, times(n)).getString(ID_COLUMN)
     }
 }
